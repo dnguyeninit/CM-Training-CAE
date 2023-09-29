55599a067d8549b3fbddb99363387fb4dfc7cc46
Duc Nguyen <duc.nguyen@init.de>
2023-09-29 15:40:55 +0200

    CM Enablement CAE: created request handling method without page handler. Pdf views are rendered correctly. When view==null, then the CMVideoTutorial will be rendered as a fragment instead of a page

--- apps/cae/modules/extensions/training/training-cae/src/main/java/com/coremedia/blueprint/training/handlers/VideoTutorialHandler.java
+++ apps/cae/modules/extensions/training/training-cae/src/main/java/com/coremedia/blueprint/training/handlers/VideoTutorialHandler.java
@@ -1,7 +1,12 @@
 package com.coremedia.blueprint.training.handlers;
 
 import com.coremedia.blueprint.training.contentbeans.CMVideoTutorial;
+import com.coremedia.objectserver.web.HandlerHelper;
 import com.coremedia.objectserver.web.links.Link;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +21,7 @@ import java.util.Map;
 * */
 
 @Link
+@RequestMapping
 public class VideoTutorialHandler {
 
   private static final String URL_PATTERN = "/videotutorial/{id:\\d+}/{title}.{extension}";
@@ -49,6 +55,24 @@ public class VideoTutorialHandler {
 
   private static String getExtension(String view) {
     return view == null ? "html" : view;
+  }
+
+  @GetMapping(URL_PATTERN)
+  public ModelAndView handleRequest(@PathVariable("id") CMVideoTutorial self,
+                                    @PathVariable("extension") String extension) {
+    if (self == null) {
+      return HandlerHelper.notFound();
+    }
+
+    String view = getViewFromExtension(extension);
+
+    ModelAndView mav = HandlerHelper.createModelWithView(self, view);
+    return mav;
+  }
+
+  private String getViewFromExtension(String extension) {
+//    return ("html".equals(extension) ? null : extension);
+    return (extension.equals("html") ? null : extension);
   }
 
 }
